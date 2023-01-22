2619fe118182870e32a8e68aad94a79000c6aab6
lalmastar <107454522+lalmastar@users.noreply.github.com>
2023-01-22 19:48:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,22 +11,64 @@ app.use(express.json());
 
 // Get all the students
 app.get('/students', async (req, res) => {
-    // write your codes here
+    try {
+        const allStudents = await Student.find({isDeleted: false});
+        if(!allStudents) return res.status(404).json(404);
+        return res.json(allStudents);
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
 })
 
 // Add student to database
 app.post('/students', async (req, res) =>{
-    // write your codes here
+    const student = new Student({
+        name: req.body.name,
+        sex: req.body.sex,
+        age: req.body.age,
+        class: req.body.class,
+        grade_point: req.body.grade_point,
+    })
+
+    try{
+        const newSavedStudent = await student.save();
+        return res.json(newSavedStudent);
+    }catch(error){
+        return res.status(500).json(error.message);
+    }
 })
 
 // Get specific student
 app.get('/students/:id', async (req, res) =>{
-    // write your codes here
+    try {
+        const allStudents = await Student.findOne({_id: req.params.id, isDeleted: false});
+        if(allStudents === null) return res.sendStatus(404);
+        return res.json(allStudents);
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
 })
 
 // delete specific student
 app.delete('/students/:id', async (req, res) =>{
-    // write your codes here
+    if(req.query.type && req.query.type === 'hard'){
+        try {
+            const removedStudent = await Student.deleteOne({_id: req.params.id});
+            if(removedStudent.deletedCount === 1)
+                return res.json(removedStudent);
+            else
+                return res.sendStatus(404);
+        } catch (error) {
+            return res.status(500).json({message: error.message})
+        }
+    } else{
+        try {
+            const removedStudent = await Student.updateOne({_id: req.params.id}, {isDeleted: true});
+            return res.json(removedStudent);
+        } catch (error) {
+            return res.status(500).json({message: error.message})
+        }
+    }
 }) 
 
 
